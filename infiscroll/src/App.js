c8012f6c2bc9044c6bafc7435d44ab11e1432492
sbhusal123 <suryabhusal11@gmail.com>
2021-04-16 10:02:06 +0545

    Github deploy issue fixes.

--- infiscroll/src/App.js
+++ infiscroll/src/App.js
@@ -1,33 +1,12 @@
-import ClassBasedPaginationScroll from "./Components/ClassBasedPaginationScroll";
 import FunctionalPaginationScroll from "./Components/FunctionalPaginationScroll";
-
-import { Col, Container, Button } from "react-bootstrap";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 const App = () => {
-    const url = window.location.href;
     return (
         <Provider store={store}>
-            {url.includes("autoload") ? <ClassBasedPaginationScroll /> : null}
-            {url.includes("manualload") ? <FunctionalPaginationScroll /> : null}
-
-            {!url.includes("autoload") && !url.includes("manualload") ? (
-                <Container style={{ marginTop: "5%" }}>
-                    <Col>
-                        <Button href={url + "/autoload/"}>
-                            Demo : Autoload with Scroll
-                        </Button>
-                    </Col>
-                    <br></br>
-                    <Col>
-                        <Button href={url + "/manualload/"}>
-                            Demo: Load more on end
-                        </Button>
-                    </Col>
-                </Container>
-            ) : null}
+            <FunctionalPaginationScroll />
         </Provider>
     );
 };
